83f306ba7285d9861ec74f9809fba3b2f72a4ee0
ZuuZaa <zumbaghirova@gmail.com>
2019-04-06 19:27:48 +0400

    initial

---
+++ xemse.js
@@ -0,0 +1,153 @@
+var meyveler = [
+
+    {
+        sual: "Bu meyvənin elmi adı iki latın sözünün birləşməsindən əmələ gəlmişdir. Punica - tünd qırmızı və granatus - dənə deməkdir.",
+        cavab: [{ name: "nar", value: true },
+        { name: "üzüm", value: false },
+        { name: "qarağat", value: false },
+        { name: "şaftalı", value: false }]
+    },
+    {
+        sual: "Azərbaycanda bu meyvənin əti məcməilərə yayılaraq günəşdə qurudulur və ondan”lavaş” hazırlanılır.",
+        cavab: [{ name: "üzüm", value: false },
+        { name: "armud", value: false },
+        { name: "nar", value: false },
+        { name: "alça", value: true }]
+    },
+    {
+        sual: "Avropalılar orta əsrlərdə Çin dövlətinin məmurlarını bu bitkinin adı ilə adlandırırdılar.",
+        cavab: [{ name: "Mandarin", value: true },
+        { name: "Banan", value: false },
+        { name: "Alma", value: false },
+        { name: "Kivi", value: false }]
+    },
+    {
+        sual: "Bu meyvənin hərfi mənada tərcüməsi - Fars gavalısı deməkdir",
+        cavab: [{ name: "Ərik", value: false },
+        { name: "Albalı", value: false },
+        { name: "Şaftalı", value: true },
+        { name: "Moruq", value: false }]
+    },
+    {
+        sual: "1801 - ci ildə Portuqaliya və İspaniya arasında olmuş müharibə",
+        cavab: [{ name: "Portağal", value: true },
+        { name: "Heyva", value: false },
+        { name: "Armud", value: false },
+        { name: "Göyəm", value: false }]
+    }]
+var heyvanlar = [
+    {
+        sual: " Avstraliyanın cənub sahillərində yerləşən ada",
+        cavab: [{ name: "Ağ ayı", value: false },
+        { name: "İlan", value: false },
+        { name: "Kenquru", value: true },
+        { name: "Maral", value: false }]
+    },
+    {
+        sual: "Bu qitənin adının mənası - böyük ayı bürcünün əksində deməkdir.",
+        cavab: [{ name: "Avropa", value: false },
+        { name: "Amerika", value: false },
+        { name: "Antarktida", value: true },
+        { name: "Asiya", value: false }]
+    },
+    {
+        sual: "Timsahlı çay - mənasını verən Mali dövlətinin paytaxtı bu şəhərdir",
+        cavab: [{ name: "Bali", value: false },
+        { name: "Birma", value: false },
+        { name: "Bamako", value: true },
+        { name: "Butan", value: false }]
+    },
+    {
+        sual: "İsveçrənin paytaxt şəhəri Bern alman dilindən tərcümədə bu heyvanı bildirir ",
+        cavab: [{ name: "kərtənkələ", value: false },
+        { name: "ayı", value: true },
+        { name: "arı", value: false },
+        { name: "kirpi", value: false }]
+    },
+    {
+        sual: "Lui Stoumenin 1962-ci ildə Adolf Hitlerin həyatı haqqında çəkdiyi və Oskara layiq görülmüş bu film belə adlanır",
+        cavab: [{ name: "Yalnız Canavar", value: false },
+        { name: "Zəhərli ilan", value: false },
+        { name: "Qara Tülkü", value: true },
+        { name: "Pələng", value: false }]
+    }]
+
+var maraqli = [
+    {
+        sual: " Saat əqrəbi istiqamətində hərəkət edən yeganə planet",
+        cavab: [{ name: "Mars", value: false },
+        { name: "Yupitern", value: false },
+        { name: "Venera", value: true },
+        { name: "Saturn", value: false }]
+    },
+    {
+        sual: "İlk gözəllik müsaqibəsi bu Avropa ölkəsində keçirilib",
+        cavab: [{ name: "Fransa", value: false },
+        { name: "Alamaniya", value: false },
+        { name: "Belçika", value: true },
+        { name: "İtaliiya", value: false }]
+    },
+    {
+        sual: "Kürün Xəzərə tökülən qolu belə adlanır",
+        cavab: [{ name: "Balıqlı", value: false },
+        { name: "Tökmə", value: false },
+        { name: "Aquşa", value: true },
+        { name: "Qovuşan", value: false }]
+    },
+    {
+        sual: "Bombalar və güllələr haqqında elm belə adlanır",
+        cavab: [{ name: "Talistika", value: false },
+        { name: "Balistika", value: true },
+        { name: "Bombastika", value: false },
+        { name: "Kinomaniya", value: false }]
+    },
+    {
+        sual: "Bu canlının elmi adı struthio-camelusdur",
+        cavab: [{ name: "Çaqqal", value: false },
+        { name: "Dəvə", value: false },
+        { name: "Dəvəquşu", value: true },
+        { name: "Pələng", value: false }]
+    }]
+    . Paris
+    . Cek London
+     Soçi
+    
+       Ərdəbil
+
+    var seherler = [
+
+        {
+            sual: "O, hökmdar Priamın oğlu və Toya müharibəsinin səbəbkarıdır",
+            cavab: [{ name: "Paris", value: true },
+            { name: "London", value: false },
+            { name: "Oslo", value: false },
+            { name: "Praqa", value: false }]
+        },
+        {
+            sual: "Bu amerikalı yazıçının əsl adı Con Qrafitedir",
+            cavab: [{ name: "Adam Boston", value: false },
+            { name: "Adrian Polo", value: false },
+            { name: "Jon Smit", value: false },
+            { name: "Cek London", value: true }]
+        },
+        {
+            sual: "2014-cü ilin qış olimpiya oyunları bu şəhərdə keçirildi",
+            cavab: [{ name: "Soçi", value: true },
+            { name: "Bakı", value: false },
+            { name: "Ankara", value: false },
+            { name: "Tokyo", value: false }]
+        },
+        {
+            sual: "ABŞ-n bu şəhərini - Böyük alma adlandırırlar",
+            cavab: [{ name: "Nyu Jersi", value: false },
+            { name: "Vaşinqton", value: false },
+            { name: "Nyu York", value: true },
+            { name: "Boston", value: false }]
+        },
+        {
+            sual: "XVII əsr italyan səyyahı Pyetro Dello Valle bu şəhəri - Azərbaycan Venesiyası adlandırmışsdır",
+            cavab: [{ name: "Ərdəbil", value: true },
+            { name: "Gəncə", value: false },
+            { name: "Təbriz", value: false },
+            { name: "Bakı", value: false }]
+        }]
